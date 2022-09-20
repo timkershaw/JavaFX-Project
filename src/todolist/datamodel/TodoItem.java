a368baf249a42c74fc4ae6b791c6307588a68681
timkershaw <44982931+timkershaw@users.noreply.github.com>
2022-09-19 22:33:41 -0400

    assigned a custom cell factory to the listview

--- src/todolist/datamodel/TodoItem.java
+++ src/todolist/datamodel/TodoItem.java
@@ -42,8 +42,8 @@ public class TodoItem {
         this.deadline = deadline;
     }
 
-    @Override
-    public String toString() {
-        return shortDescription;
-    }
+//    @Override
+//    public String toString() {
+//        return shortDescription;
+//    }
 }
